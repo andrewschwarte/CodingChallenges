f8225b6e963f41b4e0851495563a18260f14bd55
andrewschwarte <andrewschwarte@gmail.com>
2025-01-10 09:08:22 -0500

    coding challenge for 01-10-2025

---
+++ CC2025-01-10/app.js
@@ -0,0 +1,15 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//P: we will recieve a bool aka true or false but not a string 
+//R: return a string "true" if true or a sting "false" if false
+//E: true "true"
+//   false "false"
+//P: create a function that takes in a bool(true or false), if true return "true"
+
+function booleanToString(b){
+    return (b === true) ? "true" : "false"
+}
+
+//ez
