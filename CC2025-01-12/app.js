9e3285544ec6729a1f6e5eaed99df1041adc0763
andrewschwarte <andrewschwarte@gmail.com>
2025-01-12 11:06:55 -0500

    coding challenge for 01-12-2025

---
+++ CC2025-01-12/app.js
@@ -0,0 +1,41 @@
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+//P; find and replace. Input is a string of 'A''T''C''G' does length matter?
+//R: returns the complenet of the inputed string
+//E: "CG" -> "GC"
+//   "ATTGC" --> "TAACG"
+//P: create a function that takes in a string of ATCG. Replace A with T and G with C and vice versa.
+
+function dnaStrand(dna){
+    let dnaArr = dna.split("")
+    for(let i = 0; i < dnaArr.length; i++){
+        switch(dnaArr[i]){
+            case 'A':
+                dnaArr[i] = 'T'
+            break;
+            case 'T':
+                dnaArr[i] = 'A'
+            break;
+            case 'C':
+                dnaArr[i] = 'G'
+            break;
+            case 'G':
+                dnaArr[i] = 'C'
+            break;
+        }
+    }
+    return dnaArr.join("")
+}
+console.log(dnaStrand("ATTGC"), "TAACG")
+
