d48308ff699ee1a74790bcce96478b5cc5265d3e
andrewschwarte <andrewschwarte@gmail.com>
2025-02-04 16:03:42 -0500

    coding challenge for 02-04-2025

--- CC2025-02-04/app.js
+++ CC2025-02-04/app.js
@@ -12,19 +12,4 @@ function isSingleton(str){
 
 console.log(isSingleton('ssss'))
 
-function isArithmetic(str){
-    
-}
 
-function isPalindrome(str){
-    let j = str.length - 1
-    for(let i = 0; i < j / 2; i++){
-        let x = str[i]
-        let y = str[j - i]
-        if(x != y)
-            return false
-    }   
-    return true
-}
-
-console.log(isPalindrome('racecar'))
