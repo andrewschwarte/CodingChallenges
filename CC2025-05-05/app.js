4248593f0df21ac0b1b87546769c505d6b04cd54
andrewschwarte <andrewschwarte@gmail.com>
2025-05-05 20:21:00 -0400

    add challenge for May 5

---
+++ CC2025-05-05/app.js
@@ -0,0 +1,15 @@
+// String-1 --
+
+// firstHalf
+
+// Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+// Examples
+
+//     firstHalf('WooHoo') → WooHoo
+//     firstHalf('HelloThere') → HelloThere
+//     firstHalf('abcdefg') → abcdefg
+
+function firstHalf(str) {
+  return str.substring(0, str.length / 2);
+}
