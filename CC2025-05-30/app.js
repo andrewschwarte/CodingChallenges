c230add8543c8dd7671f663fdafc7885dc19d7dd
andrewschwarte <andrewschwarte@gmail.com>
2025-05-30 20:38:15 -0400

    add challenge for May 30

---
+++ CC2025-05-30/app.js
@@ -0,0 +1,20 @@
+// String-1 --
+
+// withoutX
+
+// Given a string, if the first or last chars are 'x', return the string without those 'x' chars, and otherwise return the string unchanged.
+
+// Examples
+
+//     withoutX('xHix') → Hi
+//     withoutX('xHi') → Hi
+//     withoutX('Hxix') → Hxi
+
+function withoutX(str) {
+  if (str.length == 1) return "";
+  if (str.startsWith("x") && str.endsWith("x"))
+    return str.substring(1, str.length - 1);
+  else if (str.startsWith("x")) return str.substring(1, str.length);
+  else if (str.endsWith("x")) return str.substring(0, str.length - 1);
+  else return str;
+}
