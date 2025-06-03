23b32e78de8e3784ad4a96429f480737d902316f
andrewschwarte <andrewschwarte@gmail.com>
2025-06-03 12:00:00 -0400

    Add coding challenge for 2025-06-03

---
+++ CC2025-06-03/app.js
@@ -0,0 +1,19 @@
+// String-2 --
+
+// countHi
+
+// Return the number of times that the string "hi" appears anywhere in the given string.
+
+// Examples
+
+//     countHi('abc hi ho') → 1
+//     countHi('ABChi hi') → 2
+//     countHi('hihi') → 2
+
+function countHi(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "h" && str[i + 1] == "i") count++;
+  }
+  return count;
+}
