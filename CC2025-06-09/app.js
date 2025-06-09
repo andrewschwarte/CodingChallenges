ff127090819f949b48fdfd81cf0035bb94a08b71
andrewschwarte <andrewschwarte@gmail.com>
2025-06-09 12:00:00 -0400

    Add coding challenge for 2025-06-09

---
+++ CC2025-06-09/app.js
@@ -0,0 +1,18 @@
+// String-2 --
+
+// bobThere
+
+// Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+
+// Examples
+
+//     bobThere('abcbob') → true
+//     bobThere('b9b') → true
+//     bobThere('bac') → false
+
+function bobThere(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "b" && str[i + 2] == "b") return true;
+  }
+  return false;
+}
