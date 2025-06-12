44c5782e5626f48b127fda024d1eac760eccdb55
andrewschwarte <andrewschwarte@gmail.com>
2025-06-12 12:00:00 -0400

    Add coding challenge for 2025-06-12

---
+++ CC2025-06-12/app.js
@@ -0,0 +1,18 @@
+// String-2 --
+
+// repeatEnd
+
+// Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+// Examples
+
+//     repeatEnd('Hello', 3) → llollollo
+//     repeatEnd('Hello', 2) → lolo
+//     repeatEnd('Hello', 1) → o
+
+function repeatEnd(str, n) {
+  let ending = str.slice(str.length - n, str.length);
+  let result = "";
+  for (let i = 0; i < n; i++) result += ending;
+  return result;
+}
