06d3588479b60f58a0f4c4470704027fd585b46e
andrewschwarte <andrewschwarte@gmail.com>
2025-06-21 12:00:00 -0400

    Add coding challenge for 2025-06-21

---
+++ CC2025-06-21/app.js
@@ -0,0 +1,29 @@
+// String-2 --
+
+// starOut
+
+// Return a version of the given string, where for every star (*) in the string the star and the chars immediately to its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
+
+// Examples
+
+//     starOut('ab*cd') → ad
+//     starOut('ab**cd') → ad
+//     starOut('sm*eilly') → silly
+
+function starOut(str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] == "*" ||
+      (i > 0 && str[i - 1] == "*") ||
+      (i < str.length - 1 && str[i + 1] == "*")
+    ) {
+      // skip this character
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
