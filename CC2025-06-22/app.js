74f3fca3280e4af983c1a245bc0dabf872720933
andrewschwarte <andrewschwarte@gmail.com>
2025-06-22 12:00:00 -0400

    Add coding challenge for 2025-06-22

---
+++ CC2025-06-22/app.js
@@ -0,0 +1,29 @@
+// String-2 --
+
+// plusOut
+
+// Given a string and a non-empty word string, return a version of the original String where all chars have been replaced by pluses ("+"), except for appearances of the word string which are preserved unchanged.
+
+// Examples
+
+//     plusOut('12xy34', 'xy') → ++xy++
+//     plusOut('12xy34', '1') → 1+++++
+//     plusOut('12xy34xyabcxy', 'xy') → ++xy++xy+++xy
+
+function plusOut(str, word) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (
+      i + word.length <= str.length &&
+      str.substring(i, i + word.length) == word
+    ) {
+      result += word;
+      i += word.length - 1;
+    } else {
+      result += "+";
+    }
+  }
+
+  return result;
+}
