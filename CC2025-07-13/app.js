4dd1e0698a93c277e0f8ad05f2ca0880306ca187
andrewschwarte <andrewschwarte@gmail.com>
2025-07-13 12:00:00 -0400

    Add coding challenge for 2025-07-13

---
+++ CC2025-07-13/app.js
@@ -0,0 +1,18 @@
+// Logic-1 --
+
+// old35
+
+// Return true if the given non-negative number is a multiple of 3 or 5, but not both. Use the % "mod" operator.
+
+// Examples
+
+//     old35(3) → true
+//     old35(10) → true
+//     old35(15) → false
+
+function old35(n) {
+  if (n % 3 == 0 && n % 5 == 0) return false;
+  else if (n % 3 == 0) return true;
+  else if (n % 5 == 0) return true;
+  else return false;
+}
