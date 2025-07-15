4cba7e87b5ddb864823a9b8aa54c7d94657b3318
andrewschwarte <andrewschwarte@gmail.com>
2025-07-15 12:00:00 -0400

    Add coding challenge for 2025-07-15

---
+++ CC2025-07-15/app.js
@@ -0,0 +1,16 @@
+// Logic-1 --
+
+// nearTen
+
+// Given a non-negative number "num", return true if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2.
+
+// Examples
+
+//     nearTen(12) → true
+//     nearTen(17) → false
+//     nearTen(19) → true
+
+function nearTen(num) {
+  if (num % 10 <= 2 || num % 10 >= 8) return true;
+  else return false;
+}
