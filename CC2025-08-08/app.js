79c2cb28c6392e0d72371288349153e8b553462a
andrewschwarte <andrewschwarte@gmail.com>
2025-08-08 12:00:00 -0400

    Add coding challenge for 2025-08-08

---
+++ CC2025-08-08/app.js
@@ -0,0 +1,19 @@
+// Logic-2 --
+
+// blackjack
+
+// Given 2 int values greater than 0, return whichever value is nearest to 21 without going over. Return 0 if they both go over.
+
+// Examples
+
+//     blackjack(19, 21) → 21
+//     blackjack(21, 19) → 21
+//     blackjack(19, 22) → 19
+
+function blackjack(a, b) {
+  if (a > 21 && b > 21) return 0;
+  else if (a > b && a <= 21) return a;
+  else if (b > a && b <= 21) return b;
+  else if (a > 21) return b;
+  else return a;
+}
