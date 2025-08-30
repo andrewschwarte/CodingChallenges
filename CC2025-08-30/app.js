553b3c301fc1b85f184630d3cdbf0ad901ed6482
andrewschwarte <andrewschwarte@gmail.com>
2025-08-30 12:00:00 -0400

    Add coding challenge for 2025-08-30

---
+++ CC2025-08-30/app.js
@@ -0,0 +1,22 @@
+// Array-1 --
+
+// plusTwo
+
+// Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+
+// Examples
+
+//     plusTwo([1, 2], [3, 4]) → 1,2,3,4
+//     plusTwo([4, 4], [2, 2]) → 4,4,2,2
+//     plusTwo([9, 2], [3, 4]) → 9,2,3,4
+
+function plusTwo(a, b) {
+  let result = [];
+  for (let i = 0; i < 2; i++) {
+    result.push(a[i]);
+  }
+  for (let i = 0; i < 2; i++) {
+    result.push(b[i]);
+  }
+  return result;
+}
