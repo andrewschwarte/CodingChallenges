5da2786caa2daff916846f4a2e662c9e40aa3141
andrewschwarte <andrewschwarte@gmail.com>
2025-09-02 12:00:00 -0400

    Add coding challenge for 2025-09-02

---
+++ CC2025-09-02/app.js
@@ -0,0 +1,22 @@
+// Array-1 --
+
+// maxTriple
+
+// Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
+
+// Examples
+
+//     maxTriple([1, 2, 3]) → 3
+//     maxTriple([1, 5, 3]) → 5
+//     maxTriple([5, 2, 3]) → 5
+
+function maxTriple(nums) {
+  let checking = [
+    nums[0],
+    nums[Math.floor(nums.length / 2)],
+    nums[nums.length - 1],
+  ];
+
+  checking = checking.sort((a, b) => a - b);
+  return checking[2];
+}
