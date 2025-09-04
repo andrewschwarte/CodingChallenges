88547c50d7902b80c43265b60aa29c03927ffdde
andrewschwarte <andrewschwarte@gmail.com>
2025-09-04 12:00:00 -0400

    Add coding challenge for 2025-09-04

---
+++ CC2025-09-04/app.js
@@ -0,0 +1,21 @@
+// Array-1 --
+
+// unlucky1
+
+// We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1. Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
+
+// Examples
+
+//     unlucky1([1, 3, 4, 5]) → true
+//     unlucky1([2, 1, 3, 4, 5]) → true
+//     unlucky1([1, 1, 1]) → false
+
+function unlucky1(nums) {
+  for (let i = 0; i < 2; i++) {
+    if (nums[i] == 1 && nums[i + 1] == 3) return true;
+  }
+  for (let i = nums.length - 2; i < nums.length; i++) {
+    if (nums[i] == 1 && nums[i + 1] == 3) return true;
+  }
+  return false;
+}
