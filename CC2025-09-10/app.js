805f7965a05292e0afc3ae4720cd401b698d28ec
andrewschwarte <andrewschwarte@gmail.com>
2025-09-10 12:00:00 -0400

    Add coding challenge for 2025-09-10

---
+++ CC2025-09-10/app.js
@@ -0,0 +1,20 @@
+// Array-2 --
+
+// centeredAverage
+
+// Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.
+
+// Examples
+
+//     centeredAverage([1, 2, 3, 4, 100]) → 3
+//     centeredAverage([1, 1, 5, 5, 10, 8, 7]) → 5.2
+//     centeredAverage([-10, -4, -2, -4, -2, 0]) → -3
+
+function centeredAverage(nums) {
+  let newArr = nums.sort((a, b) => a - b);
+  let trimed = [];
+  for (let i = 1; i < newArr.length - 1; i++) {
+    trimed.push(newArr[i]);
+  }
+  return trimed.reduce((a, b) => a + b) / trimed.length;
+}
