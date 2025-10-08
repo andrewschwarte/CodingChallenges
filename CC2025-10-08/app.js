636034214c93820d965e90f414f42788ef343805
andrewschwarte <andrewschwarte@gmail.com>
2025-10-08 12:00:00 -0400

    Add coding challenge for 2025-10-08

---
+++ CC2025-10-08/app.js
@@ -0,0 +1,22 @@
+// Array-2 --
+
+// shiftLeft
+
+// Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}. You may modify and return the given array, or return a new array.
+
+// Examples
+
+//     shiftLeft([6, 2, 5, 3]) → 2,5,3,6
+//     shiftLeft([1, 2]) → 2,1
+//     shiftLeft([1]) → 1
+
+function shiftLeft(nums) {
+  if (nums.length == 0) return [];
+  let newArr = nums.slice(1);
+
+  let firstElement = nums[0];
+
+  newArr.push(firstElement);
+
+  return newArr;
+}
