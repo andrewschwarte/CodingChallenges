9c74f86e4120f9ba153db2f285ad1226314b1723
andrewschwarte <andrewschwarte@gmail.com>
2025-10-17 12:00:00 -0400

    Add coding challenge for 2025-10-17

---
+++ CC2025-10-17/app.js
@@ -0,0 +1,34 @@
+// Array-3 --
+
+// fix45
+
+// (This is a slightly harder version of the fix34 problem.) Return an array that contains exactly the same numbers as the given array, but rearranged so that every 4 is immediately followed by a 5. Do not move the 4's, but every other number may move. The array contains the same number of 4's and 5's, and every 4 has a number after it that is not a 4. In this version, 5's may appear anywhere in the original array.
+
+// Examples
+
+//     fix45([5, 4, 9, 4, 9, 5]) → 9,4,5,4,5,9
+//     fix45([1, 4, 1, 5]) → 1,4,5,1
+//     fix45([1, 4, 1, 5, 5, 4, 1]) → 1,4,5,1,1,4,5
+
+function fix45(nums) {
+  let fiveIndex = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 4) {
+      if (nums[i + 1] != 5) {
+        while (
+          nums[fiveIndex] != 5 ||
+          (fiveIndex > 0 && nums[fiveIndex - 1] == 4)
+        ) {
+          fiveIndex++;
+        }
+
+        let temp = nums[i + 1];
+        nums[i + 1] = 5;
+        nums[fiveIndex] = temp;
+      }
+    }
+  }
+
+  return nums;
+}
